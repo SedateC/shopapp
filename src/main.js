3e419ac24729d5ac1ce2f467e3db5ab86a644e65
SedateC <248694148@qq.com>
2019-12-07 16:49:45 +0800

    文件目录和网页规范

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'; //调用格式化浏览器插件
 
 Vue.config.productionTip = false
 
